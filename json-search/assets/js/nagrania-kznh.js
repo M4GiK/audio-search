58ce7e750ead9d0daa6bb2786cf7891b76bcf9c9
m4gik <m4giksoftware@gmail.com>
2015-04-09 01:49:40 +0200

    Create working audio-search for kznh

--- json-search/assets/js/nagrania-kznh.js
+++ json-search/assets/js/nagrania-kznh.js
@@ -4,6 +4,8 @@ jQuery.getJSON("assets/library/lib.json", function(data, textStatus) {
 	if (textStatus == "success") {
 		jQuery.each(data, function(key, value) {
 			if (key != "_comment") {
+				value.webdirectory = value["web-directory"];
+				delete value["web-directory"];			
 				movies.push(value);			
 			}
 		});
